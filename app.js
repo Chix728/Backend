a39254b097a37e3e0ff75e47cbcfd5d547b45216
Chix728 <chixkhan728@gmail.com>
2022-11-13 22:50:37 +0530

    implemented mongo db post method

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-const { Router } = require('express');
+const { Router, response } = require('express');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose'); //8EYD3e-r_rCRPwe  user:eshopApplication
+
 require('dotenv/config');
+//Middleware
 app.use(express.json())
+app.use(morgan('tiny'));
+
+
 //app.use(Router());
 const api_url = process.env.API_URL;
+console.log(process.env.CONNECTION_STRING)
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    dbName:'EshopDatabase'
+})
+.then(()=>{
+    console.log('Database Conected Sucessfully!!');    
+})
+.catch(error =>{
+    console.log(error);
+})
+
+const productSchema = mongoose.Schema({
+    id:Number,
+    name:String,
+    image: String,
+    countInStock: Number
+});
+
+const Products =  mongoose.model('Products',productSchema);
 app.listen(3000, ()=>{
     console.log(api_url);
     console.log("App is running on http://localhost:3000");
@@ -20,8 +48,27 @@ app.get(`${api_url}/products`,(req,res)=>{
     product);
 })
 app.post(`${api_url}/products`,(req,res)=>{
-    const newcproduct = req.body;
-    console.log(newcproduct)
-    res.send(
-     newcproduct);
+    const newProduct  = new Products(
+        {
+            id:req.body.id,
+            name:req.body.name,
+            image:req.body.image,
+            countInStock:req.body.countInStock
+        }
+    )
+    newProduct.save().then(
+       (createdProduct =>{
+            res.status(201).json(newProduct)
+       })
+    ).catch(error=>{
+        res.status(500).json({
+            error:error,
+            success:false
+        })
+    })
+    res.send(newProduct);
+    //const newcproduct = req.body;
+    //console.log(newcproduct)
+    //res.send(
+    // newcproduct);
 })
